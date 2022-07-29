23fc7b5d5a4d9ad36496523e65978b70fa87619b
tv1tty1 <bodya1999@icloud.com>
2022-07-29 15:50:09 +0300

    first commit

---
+++ Pulse/Pulse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pulse
+//
+//  Created by MacBook on 23.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
